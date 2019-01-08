333ac53e479b56a8d41bc96a5467b04fbc50de7c
許當麻 <no2don@outlook.com>
2019-01-08 16:27:08 +0800

    加入專案檔案。

---
+++ FileMoveTool/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace FileMoveTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSourceFrom = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnPreScan = new System.Windows.Forms.Button();
+            this.lblPreview = new System.Windows.Forms.Label();
+            this.dateTimePickerBase = new System.Windows.Forms.DateTimePicker();
+            this.listTargetFilesInfo = new System.Windows.Forms.ListBox();
+            this.cbCopy = new System.Windows.Forms.CheckBox();
+            this.cbDelete = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTargetDirectory = new System.Windows.Forms.Label();
+            this.btnExcute = new System.Windows.Forms.Button();
+            this.backgroundWorkerCopy = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.backgroundWorkerDelete = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSourceFrom
+            // 
+            this.btnSourceFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSourceFrom.Location = new System.Drawing.Point(6, 6);
+            this.btnSourceFrom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSourceFrom.Name = "btnSourceFrom";
+            this.btnSourceFrom.Size = new System.Drawing.Size(294, 24);
+            this.btnSourceFrom.TabIndex = 0;
+            this.btnSourceFrom.Text = "1. Select Souce From Directory";
+            this.btnSourceFrom.UseVisualStyleBackColor = true;
+            this.btnSourceFrom.Click += new System.EventHandler(this.btnSourceFrom_Click);
+            // 
+            // lblSource
+            // 
+            this.lblSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(6, 36);
+            this.lblSource.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(165, 16);
+            this.lblSource.TabIndex = 1;
+            this.lblSource.Text = "\\\\Mac\\Home\\Pictures\\Snap";
+            // 
+            // btnPreScan
+            // 
+            this.btnPreScan.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreScan.Location = new System.Drawing.Point(190, 62);
+            this.btnPreScan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPreScan.Name = "btnPreScan";
+            this.btnPreScan.Size = new System.Drawing.Size(109, 26);
+            this.btnPreScan.TabIndex = 2;
+            this.btnPreScan.Text = "2. Scan";
+            this.btnPreScan.UseVisualStyleBackColor = true;
+            this.btnPreScan.Click += new System.EventHandler(this.btnPreScan_Click);
+            // 
+            // lblPreview
+            // 
+            this.lblPreview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPreview.AutoSize = true;
+            this.lblPreview.Location = new System.Drawing.Point(6, 94);
+            this.lblPreview.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPreview.Name = "lblPreview";
+            this.lblPreview.Size = new System.Drawing.Size(13, 16);
+            this.lblPreview.TabIndex = 3;
+            this.lblPreview.Text = "-";
+            // 
+            // dateTimePickerBase
+            // 
+            this.dateTimePickerBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePickerBase.Location = new System.Drawing.Point(6, 62);
+            this.dateTimePickerBase.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePickerBase.Name = "dateTimePickerBase";
+            this.dateTimePickerBase.Size = new System.Drawing.Size(178, 23);
+            this.dateTimePickerBase.TabIndex = 4;
+            // 
+            // listTargetFilesInfo
+            // 
+            this.listTargetFilesInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listTargetFilesInfo.FormattingEnabled = true;
+            this.listTargetFilesInfo.ItemHeight = 16;
+            this.listTargetFilesInfo.Location = new System.Drawing.Point(302, 6);
+            this.listTargetFilesInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listTargetFilesInfo.Name = "listTargetFilesInfo";
+            this.listTargetFilesInfo.Size = new System.Drawing.Size(380, 388);
+            this.listTargetFilesInfo.TabIndex = 5;
+            // 
+            // cbCopy
+            // 
+            this.cbCopy.AutoSize = true;
+            this.cbCopy.Location = new System.Drawing.Point(9, 215);
+            this.cbCopy.Name = "cbCopy";
+            this.cbCopy.Size = new System.Drawing.Size(57, 20);
+            this.cbCopy.TabIndex = 6;
+            this.cbCopy.Text = "Copy";
+            this.cbCopy.UseVisualStyleBackColor = true;
+            // 
+            // cbDelete
+            // 
+            this.cbDelete.AutoSize = true;
+            this.cbDelete.Location = new System.Drawing.Point(9, 241);
+            this.cbDelete.Name = "cbDelete";
+            this.cbDelete.Size = new System.Drawing.Size(106, 20);
+            this.cbDelete.TabIndex = 6;
+            this.cbDelete.Text = "Delete Source";
+            this.cbDelete.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "3. Select Copy Target Directory ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTargetDirectory
+            // 
+            this.lblTargetDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTargetDirectory.AutoSize = true;
+            this.lblTargetDirectory.Location = new System.Drawing.Point(6, 173);
+            this.lblTargetDirectory.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTargetDirectory.Name = "lblTargetDirectory";
+            this.lblTargetDirectory.Size = new System.Drawing.Size(13, 16);
+            this.lblTargetDirectory.TabIndex = 8;
+            this.lblTargetDirectory.Text = "-";
+            // 
+            // btnExcute
+            // 
+            this.btnExcute.Location = new System.Drawing.Point(6, 278);
+            this.btnExcute.Name = "btnExcute";
+            this.btnExcute.Size = new System.Drawing.Size(288, 23);
+            this.btnExcute.TabIndex = 7;
+            this.btnExcute.Text = "4. Excute Action";
+            this.btnExcute.UseVisualStyleBackColor = true;
+            this.btnExcute.Click += new System.EventHandler(this.btnExcute_Click);
+            // 
+            // backgroundWorkerCopy
+            // 
+            this.backgroundWorkerCopy.WorkerReportsProgress = true;
+            this.backgroundWorkerCopy.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerCopy_DoWork);
+            this.backgroundWorkerCopy.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerCopy_ProgressChanged);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(6, 304);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(13, 16);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "-";
+            // 
+            // backgroundWorkerDelete
+            // 
+            this.backgroundWorkerDelete.WorkerReportsProgress = true;
+            this.backgroundWorkerDelete.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerDelete_DoWork);
+            this.backgroundWorkerDelete.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerDelete_ProgressChanged);
+            // 
+            // lblStatus2
+            // 
+            this.lblStatus2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus2.AutoSize = true;
+            this.lblStatus2.Location = new System.Drawing.Point(6, 325);
+            this.lblStatus2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatus2.Name = "lblStatus2";
+            this.lblStatus2.Size = new System.Drawing.Size(13, 16);
+            this.lblStatus2.TabIndex = 10;
+            this.lblStatus2.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(686, 400);
+            this.Controls.Add(this.lblStatus2);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblTargetDirectory);
+            this.Controls.Add(this.btnExcute);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbDelete);
+            this.Controls.Add(this.cbCopy);
+            this.Controls.Add(this.listTargetFilesInfo);
+            this.Controls.Add(this.dateTimePickerBase);
+            this.Controls.Add(this.lblPreview);
+            this.Controls.Add(this.btnPreScan);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.btnSourceFrom);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileMoveTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSourceFrom;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnPreScan;
+        private System.Windows.Forms.Label lblPreview;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBase;
+        private System.Windows.Forms.ListBox listTargetFilesInfo;
+        private System.Windows.Forms.CheckBox cbCopy;
+        private System.Windows.Forms.CheckBox cbDelete;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTargetDirectory;
+        private System.Windows.Forms.Button btnExcute;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerCopy;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerDelete;
+        private System.Windows.Forms.Label lblStatus2;
+    }
+}
+
---
+++ FileMoveTool/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMoveTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ConcurrentBag<System.IO.FileInfo> TargetFileInfos { get; set; }
+
+        int copyFileCountSuccess { get; set; }
+        int copyFileCountFail { get; set; }
+
+        int deleteFileCountSuccess { get; set; }
+        int deleteFileCountFail { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSourceFrom_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    lblSource.Text = fbd.SelectedPath;
+                }
+
+            }
+        }
+
+
+
+        private void btnPreScan_Click(object sender, EventArgs e)
+        {
+            listTargetFilesInfo.Items.Clear();
+            TargetFileInfos = new ConcurrentBag<System.IO.FileInfo>();
+
+            GC.Collect();
+
+            if (lblSource.Text.Length <= 1)
+            {
+                MessageBox.Show("Please Select Soruce Directory", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            try
+            {
+                System.IO.DirectoryInfo SourceDI = new System.IO.DirectoryInfo(lblSource.Text);
+
+                var files = SourceDI.GetFiles();
+
+                var baseDate = dateTimePickerBase.Value.AddDays(1).AddMilliseconds(-1);
+
+                var i = 0;
+
+                Parallel.ForEach(files, (fileInfo) =>
+                {
+                    var lastWriteDate = System.IO.File.GetLastWriteTime(fileInfo.FullName);
+                    if (lastWriteDate <= baseDate)
+                    {
+                        i++;
+                        listTargetFilesInfo.Items.Add(fileInfo.FullName);
+                        TargetFileInfos.Add(fileInfo);
+                    }
+                   
+                });
+
+
+                lblPreview.Text = TargetFileInfos.Count.ToString() + " Files Matches";
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    lblTargetDirectory.Text = fbd.SelectedPath;
+                }
+
+            }
+        }
+
+        private void btnExcute_Click(object sender, EventArgs e)
+        {
+
+            lblStatus.Text = "-";
+            lblStatus2.Text = "-";
+            if (cbCopy.Checked)
+            {
+                if (lblTargetDirectory.Text.Length <= 1)
+                {
+                    MessageBox.Show("Please Select Target Directory", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            if (listTargetFilesInfo.Items.Count <= 0)
+            {
+                MessageBox.Show("Please Scan Soruce Directory", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            if (lblSource.Text == lblTargetDirectory.Text)
+            {
+                MessageBox.Show("Souce and Target could not be the same.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+
+            if (!cbCopy.Checked && !cbDelete.Checked)
+            {
+                MessageBox.Show("Please Select Copy or Delete Action", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            this.btnExcute.Enabled = false;
+
+            if (cbCopy.Checked)
+            {
+                backgroundWorkerCopy.RunWorkerAsync();
+            }
+            else if (!cbCopy.Checked && cbDelete.Checked)
+            {
+                backgroundWorkerDelete.RunWorkerAsync();
+            }
+
+
+        }
+
+
+
+        private void backgroundWorkerCopy_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            copyFileCountSuccess = 0;
+            copyFileCountFail = 0;
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            Parallel.ForEach(TargetFileInfos, (fileInfo) =>
+            {
+                try
+                {
+                    System.IO.File.Copy(fileInfo.FullName, lblTargetDirectory.Text + System.IO.Path.DirectorySeparatorChar + fileInfo.Name, true);
+                    copyFileCountSuccess++;
+                    worker.ReportProgress(copyFileCountSuccess);
+                }
+                catch
+                {
+                    copyFileCountFail++;
+                }
+            });
+
+            MessageBox.Show("Success Copy " + copyFileCountSuccess + " files, " + copyFileCountFail + " files fail .", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            if (!cbDelete.Checked)
+            {
+                this.btnExcute.Enabled = true;
+
+                return;
+            }
+
+
+
+
+            backgroundWorkerDelete.RunWorkerAsync();
+
+
+        }
+
+        private void backgroundWorkerCopy_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            lblStatus.Text = "Copy Files: " + e.ProgressPercentage.ToString() + "/" + TargetFileInfos.Count;
+        }
+
+        private void backgroundWorkerDelete_DoWork(object sender, DoWorkEventArgs e)
+        {
+            deleteFileCountSuccess = 0;
+            deleteFileCountFail = 0;
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            Parallel.ForEach(TargetFileInfos, (fileInfo) =>
+            {
+                try
+                {
+                    System.IO.File.Delete(fileInfo.FullName);
+                    deleteFileCountSuccess++;
+                    worker.ReportProgress(deleteFileCountSuccess);
+                }
+                catch
+                {
+                    deleteFileCountFail++;
+                }
+            });
+
+            this.btnExcute.Enabled = true;
+
+            MessageBox.Show("Success Delete " + deleteFileCountSuccess + " files, " + deleteFileCountFail + " files fail .", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            listTargetFilesInfo.Items.Clear();
+
+        }
+
+        private void backgroundWorkerDelete_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblStatus2.Text = "Deleted Files: " + e.ProgressPercentage.ToString() + "/" + TargetFileInfos.Count;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
